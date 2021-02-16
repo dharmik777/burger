9a09ea4ef62f7721620ccb8ccffa2b52f534a2d1
Mona Lisa <dharmikpatel7@hotmail.com>
2021-02-15 21:36:27 -0500

    .

--- db/seeds.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO burgers (burger_name, devoured) VALUES ('whopper', false);
-INSERT INTO burgers (burger_name, devoured) VALUES ('waygu Burger', false);
-INSERT INTO burgers (burger_name, devoured) VALUES ('black and bleu burger', false);
-INSERT INTO burgers (burger_name, devoured) VALUES ('cheesburger', false);
-INSERT INTO burgers (burger_name, devoured) VALUES ('hamburger', false)
