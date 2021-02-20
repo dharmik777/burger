342a3f8c2052b5716e273f3fe2eb5b2bab10adcc
Mona Lisa <dharmikpatel7@hotmail.com>
2021-02-19 23:14:47 -0500

    .

--- db/seeds.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO burgers (burger_name, devoured) VALUES ('mcRib', false);
-INSERT INTO burgers (burger_name, devoured) VALUES ('california burger', false);
-INSERT INTO burgers (burger_name, devoured) VALUES ('bufalo burger', false);
-INSERT INTO burgers (burger_name, devoured) VALUES ('shake shack schroom', false);
-INSERT INTO burgers (burger_name, devoured) VALUES ('five guys burger', false)
--- readme.md
+++
@@ -1,9 +0,0 @@
-Eat Da Burger 
-
-Enter a name of a burger in the box 
-
-Devour it
-
-Delete it
-
-Do it again!
