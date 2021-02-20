9c3e20edaa2d78dc1d993579017a02c53854213b
Mona Lisa <dharmikpatel7@hotmail.com>
2021-02-19 23:28:46 -0500

    .

---
+++ db/seeds.sql
@@ -0,0 +1,10 @@
+
+###seeds 
+
+INSERT INTO burgers (burger_name) VALUES ('Chickpea Burger');
+INSERT INTO burgers (burger_name) VALUES ('Masala Burger');
+INSERT INTO burgers (burger_name) VALUES ('Beet Burger');
+INSERT INTO burgers (burger_name) VALUES ('California Burger');
+INSERT INTO burgers (burger_name) VALUES ('Quinoa Burger');
+INSERT INTO burgers (burger_name) VALUES ('Black Bean Burger'):
+INSERT INTO burgers (burger_name) VALUES ('Sweet Potato');
