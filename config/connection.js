17b1fc3cb318ddd64ab4613ddbcbf1ea8ecb8e79
Mona Lisa <dharmikpatel7@hotmail.com>
2021-02-21 16:42:56 -0500

    .

--- config/connection.js
+++ config/connection.js
@@ -4,11 +4,11 @@ if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else{
 connection = mysql.createConnection({
-  host: "us-cdbr-east-03.cleardb.com",
+  host: "127.0.0.1",
   port: 3306,
-  user: "rb7d3c94bd68070",
-  password: "1f9310c7",
-  database: "burgerappdatabase"
+  user: "root",
+  password: "password",
+  database: "Local"
 });
 };
 
